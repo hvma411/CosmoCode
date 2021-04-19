6361ca0c325668bad43a3c5cbcc06f64205da3cb
hvma411 <kamil.nosek411@gmail.com>
2021-04-19 21:49:31 +0200

    hamburger menu btn with full animations and new svg icons made for webiste

--- src/components/MenuComponent/MenuComponent.js
+++ src/components/MenuComponent/MenuComponent.js
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MenuComponent = () => {
+
+    const [isMenuVisible, setIsMenuVisible] = useState(true);
+
     return (
         <nav className="nav-sidebar">
+            <HamburgerMenuBtn 
+                isMenuVisible={ isMenuVisible } 
+                setIsMenuVisible={ setIsMenuVisible }
+            />
             <ul className="nav-wrapper">
                 <li className="nav-item">
                     <i className="home-ico"></i>
                     <span>HOME</span>
                 </li>
                 <li className="nav-item">
-                    <i className="portfolio-ico"></i>
-                    <span>PROJECTS</span>
-                </li>
-                <li className="nav-item">
                     <i className="about-ico"></i>
                     <span>ABOUT</span>
+                </li>
+                <li className="nav-item">
+                    <i className="portfolio-ico"></i>
+                    <span>PROJECTS</span>
                 </li>
                 <li className="nav-item">
                     <i className="contact-ico"></i>
                     <span>CONTACT</span>
                 </li>
             </ul>
-            <div className="nav-sidebar--line"></div>
+            {/* <div className="nav-sidebar--line"></div> */}
         </nav>
+    )
+}
+
+const HamburgerMenuBtn = ({ isMenuVisible, setIsMenuVisible }) => {
+
+
+    return (
+        <div className={`hamburger-menu--btn ${ isMenuVisible ? "hamburger--active" : "hamburger--inActive"}`}
+            onClick={() => setIsMenuVisible(!isMenuVisible)}>
+
+            <div className={`hamburger__inner`}></div>
+        </div>
     )
 }
 
