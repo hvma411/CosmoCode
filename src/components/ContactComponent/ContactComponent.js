1606375d3cb8ee48ccf822ab9f780a82cf7b92f6
hvma411 <kamil.nosek411@gmail.com>
2021-05-09 11:15:51 +0200

    small update in about component and contanct component

--- src/components/ContactComponent/ContactComponent.js
+++ src/components/ContactComponent/ContactComponent.js
@@ -7,6 +7,7 @@ const ContactComponent = () => {
         <div className="contact-component">
             <div className="contact-info-wrapper content-wrapper">
                 <div className="contact-details-box">
+                    <h2>Are you interested in to work with me?</h2>
                     <h2>Let's talk about your idea and make it real</h2>
                 </div>
             </div>
