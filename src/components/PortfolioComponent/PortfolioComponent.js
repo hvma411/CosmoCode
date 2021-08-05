0a8206e7899b566c6f3c92076bb62b789cfef5cb
hvma411 <kamil.nosek411@gmail.com>
2021-08-05 16:32:47 +0200

    another part of rwd improvement

--- src/components/PortfolioComponent/PortfolioComponent.js
+++ src/components/PortfolioComponent/PortfolioComponent.js
@@ -34,89 +34,92 @@ const PortfolioComponent = () => {
     )
 }
 
+const projects = {
+    web: [
+        {
+            id: 1,
+            name: "Zielonojemy",
+            fileName: "ZielonojemyDesktopMockup.png",
+            mobileFileName: "ZielonojemyMobileMockup.png",
+            type: "web",
+            projectName: ProjectName.ZIELONOJEMY,
+            projectTechStack: [
+                "React",
+                "Firebase",
+                "Scss"
+            ],
+        },
+        {
+            id: 2,
+            name: "CMS",
+            fileName: "PanelZielonojemyDesktopMockup.png",
+            type: "web",
+            projectName: ProjectName.CMS,
+            projectTechStack: [
+                "React",
+                "Firebase",
+                "Scss"
+            ],
+        },
+        {
+            id: 3,
+            name: "Best Shop",
+            fileName: "BestShopDesktopMockup.png",
+            mobileFileName: "BestShopMobileMockup.png",
+            type: "web",
+            projectName: ProjectName.BEST_SHOP,
+            projectTechStack: [
+                "React",
+                "Firebase",
+                "Scss"
+            ],
+        },
+        {
+            id: 4,
+            name: "CosmoCode",
+            fileName: "CosmoCodeDesktopMockup.png",
+            mobileFileName: "CosmoCodeMobileMockup.png",
+            type: "web",
+            projectName: ProjectName.COSMO_CODE,
+            projectTechStack: [
+                "React",
+                "Firebase",
+                "Scss"
+            ],
+        }
+    ],
+    art: [
+        {
+            id: 1,
+            fileName: "art1.jpg",
+            type: "art",
+            projectName: ProjectName.PYRAMIDS,
+        },
+        {
+            id: 2,
+            fileName: "art2.jpg",
+            type: "art",
+            projectName: ProjectName.SAND_DUNES
+        },
+        {
+            id: 3,
+            fileName: "art3.jpg",
+            type: "art",
+            projectName: ProjectName.ECLIPSE
+        },
+        {
+            id: 4,
+            fileName: "art4.jpg",
+            type: "art",
+            projectName: ProjectName.MOUNTAINS
+        }
+    ]
+}
+
 const MobileView = () => {
     const [projectsType, setProjectsType] = useState("web");
     const [projectsPerPage, setProjectsPerPage] = useState(2);
     const [currentPage, setCurrentPage] = useState(1);
-
-    const projects = {
-        web: [
-            {
-                id: 1,
-                name: "Zielonojemy",
-                fileName: "ZielonojemyDesktopMockup.png",
-                type: "web",
-                projectName: ProjectName.ZIELONOJEMY,
-                projectTechStack: [
-                    "React",
-                    "Firebase",
-                    "Scss"
-                ],
-            },
-            {
-                id: 2,
-                name: "CMS",
-                fileName: "BestShopDesktopMockup.png",
-                type: "web",
-                projectName: ProjectName.CMS,
-                projectTechStack: [
-                    "React",
-                    "Firebase",
-                    "Scss"
-                ],
-            },
-            {
-                id: 3,
-                name: "Best Shop",
-                fileName: "web3",
-                type: "web",
-                projectName: ProjectName.BEST_SHOP,
-                projectTechStack: [
-                    "React",
-                    "Firebase",
-                    "Scss"
-                ],
-            },
-            {
-                id: 4,
-                name: "CosmoCode",
-                fileName: "web4",
-                type: "web",
-                projectName: ProjectName.COSMO_CODE,
-                projectTechStack: [
-                    "React",
-                    "Firebase",
-                    "Scss"
-                ],
-            }
-        ],
-        art: [
-            {
-                id: 1,
-                fileName: "art1",
-                type: "art",
-                projectName: ProjectName.PYRAMIDS,
-            },
-            {
-                id: 2,
-                fileName: "art2",
-                type: "art",
-                projectName: ProjectName.SAND_DUNES
-            },
-            {
-                id: 3,
-                fileName: "art3",
-                type: "art",
-                projectName: ProjectName.ECLIPSE
-            },
-            {
-                id: 4,
-                fileName: "art4",
-                type: "art",
-                projectName: ProjectName.MOUNTAINS
-            }
-        ]
-    }
 
     const nextPage = () => {
         if (currentPage === projects[projectsType].length / projectsPerPage) {
@@ -191,58 +194,24 @@ const TabletView = () => {
             <div className="projects-wrapper">
                 {projectsType === "web" ?
                     <>
-                        <div className="projects-column">
-                            <div className="project-box web-box">
-                                <div className="desktop-mockup web1"></div>
-                                <div className="mobile-mockup web1-mobile"></div>
-                                <ProjectHoverComponent projectName={ProjectName.ZIELONOJEMY} />
-
-                            </div>
-                            <div className="project-box web-box">
-                                <div className="desktop-mockup web2"></div>
-                                <ProjectHoverComponent projectName={ProjectName.CMS} />
-
-                            </div>
-                        </div>
-                        <div className="projects-column reverse-column">
-                            <div className="project-box web-box">
-                                <div className="desktop-mockup web3"></div>
-                                <div className="mobile-mockup web3-mobile"></div>
-                                <ProjectHoverComponent projectName={ProjectName.BEST_SHOP} />
-
-                            </div>
-                            <div className="project-box web-box">
-                                <div className="desktop-mockup web4"></div>
-                                <div className="mobile-mockup web4-mobile"></div>
-                                <ProjectHoverComponent projectName={ProjectName.COSMO_CODE} />
-
-                            </div>
-                        </div>
+                        <ul className="projects-column">
+                            <Projects projects={projects.web.slice(0, 2)} />
+                        </ul>
+                        <ul className="projects-column reverse-column">
+                            <Projects projects={projects.web.slice(2)} />
+                        </ul>
                     </>
                     :
                     <>
-                        <div className="projects-column">
-                            <div className="project-box">
-                                <div className="art art1"></div>
-                                <ProjectHoverComponent projectName={ProjectName.PYRAMIDS} />
-                            </div>
-                            <div className="project-box">
-                                <div className="art art2"></div>
-                                <ProjectHoverComponent projectName={ProjectName.SAND_DUNES} />
-                            </div>
-                        </div>
-                        <div className="projects-column reverse-column">
-                            <div className="project-box">
-                                <div className="art art3"></div>
-                                <ProjectHoverComponent projectName={ProjectName.ECLIPSE} />
-                            </div>
-                            <div className="project-box">
-                                <div className="art art4"></div>
-                                <ProjectHoverComponent projectName={ProjectName.MOUNTAINS} />
-                            </div>
-                        </div>
+                        <ul className="projects-column">
+                            <Projects projects={projects.art.slice(0, 2)} />
+                        </ul>
+                        <ul className="projects-column reverse-column">
+                            <Projects projects={projects.art.slice(2)} />
+                        </ul>
                     </>
                 }
+
             </div>
             <ul className="projects-nav">
                 <li className={`nav-item ${projectsType === "web" ? "active-ico" : ""}`} id="web" onClick={changeType}>
@@ -259,54 +228,108 @@ const TabletView = () => {
 }
 
 const DesktopView = () => {
+
+    const getProjectTechStack = (name) => {
+        return projects.web.filter(e => e.projectName === name)
+            .shift()
+            .projectTechStack
+    }
+
     return (
         <div className="projects-wrapper">
             <div className="projects-column">
                 <div className="project-box web-box">
-                    <div className="desktop-mockup web1"></div>
-                    <div className="mobile-mockup web1-mobile"></div>
+                    <div className="text-box">
+                        <h2>{ProjectName.ZIELONOJEMY}</h2>
+                        <ul className="tech-stack-list">
+                            {getProjectTechStack(ProjectName.ZIELONOJEMY)
+                                .map((el, idx) => (
+                                    <li key={idx}>
+                                        {el}
+                                    </li>
+                                ))}
+                        </ul>
+                    </div>
+                    <div className="mockups-wrapper">
+                        <img src="./src/utils/assets/ZielonojemyDesktopMockup.png" className="desktop-image" />
+                        <img src="./src/utils/assets/ZielonojemyMobileMockup.png" className="mobile-image" />
+                    </div>
                     <ProjectHoverComponent projectName={ProjectName.ZIELONOJEMY} />
                 </div>
                 <div className="project-box">
-                    <div className="art art1"></div>
+                    <img src={`./src/utils/assets/art1.jpg`} className="art-image" />
                     <ProjectHoverComponent projectName={ProjectName.PYRAMIDS} />
 
                 </div>
             </div>
             <div className="projects-column reverse-column">
                 <div className="project-box">
-                    <div className="art art2"></div>
+                    <img src={`./src/utils/assets/art2.jpg`} className="art-image" />
                     <ProjectHoverComponent projectName={ProjectName.SAND_DUNES} />
-
                 </div>
                 <div className="project-box web-box">
-                    <div className="desktop-mockup web2"></div>
+                    <div className="text-box">
+                        <h2>{ProjectName.CMS}</h2>
+                        <ul className="tech-stack-list">
+                            {getProjectTechStack(ProjectName.CMS)
+                                .map((el, idx) => (
+                                    <li key={idx}>
+                                        {el}
+                                    </li>
+                                ))}
+                        </ul>
+                    </div>
+                    <div className="mockups-wrapper">
+                        <img src="./src/utils/assets/PanelZielonojemyDesktopMockup.png" className="desktop-image" />
+                    </div>
                     <ProjectHoverComponent projectName={ProjectName.CMS} />
-
                 </div>
             </div>
             <div className="projects-column">
                 <div className="project-box web-box">
-                    <div className="desktop-mockup web3"></div>
-                    <div className="mobile-mockup web3-mobile"></div>
+                    <div className="text-box">
+                        <h2>{ProjectName.BEST_SHOP}</h2>
+                        <ul className="tech-stack-list">
+                            {getProjectTechStack(ProjectName.BEST_SHOP)
+                                .map((el, idx) => (
+                                    <li key={idx}>
+                                        {el}
+                                    </li>
+                                ))}
+                        </ul>
+                    </div>
+                    <div className="mockups-wrapper">
+                        <img src="./src/utils/assets/BestShopDesktopMockup.png" className="desktop-image" />
+                        <img src="./src/utils/assets/BestShopMobileMockup.png" className="mobile-image" />
+                    </div>
                     <ProjectHoverComponent projectName={ProjectName.BEST_SHOP} />
-
                 </div>
                 <div className="project-box">
-                    <div className="art art3"></div>
+                    <img src={`./src/utils/assets/art3.jpg`} className="art-image" />
                     <ProjectHoverComponent projectName={ProjectName.ECLIPSE} />
-
                 </div>
             </div>
             <div className="projects-column reverse-column">
                 <div className="project-box">
-                    <div className="art art4"></div>
+                    <img src={`./src/utils/assets/art4.jpg`} className="art-image" />
                     <ProjectHoverComponent projectName={ProjectName.MOUNTAINS} />
-
                 </div>
                 <div className="project-box web-box">
-                    <div className="desktop-mockup web4"></div>
-                    <div className="mobile-mockup web4-mobile"></div>
+                    <div className="text-box">
+                        <h2>{ProjectName.COSMO_CODE}</h2>
+                        <ul className="tech-stack-list">
+                            {getProjectTechStack(ProjectName.COSMO_CODE)
+                                .map((el, idx) => (
+                                    <li key={idx}>
+                                        {el}
+                                    </li>
+                                ))}
+                        </ul>
+                    </div>
+                    <div className="mockups-wrapper">
+                        <img src="./src/utils/assets/CosmoCodeDesktopMockup.png" className="desktop-image" />
+                        <img src="./src/utils/assets/CosmoCodeMobileMockup.png" className="mobile-image" />
+                    </div>
                     <ProjectHoverComponent projectName={ProjectName.COSMO_CODE} />
 
                 </div>
--- src/components/PortfolioComponent/Projects.js
+++ src/components/PortfolioComponent/Projects.js
@@ -1,25 +1,28 @@
 import React from 'react'
 import { ProjectHoverComponent } from './ProjectHoverComponent'
 
-export const Projects = ({ projects, projectName }) => {
+export const Projects = ({ projects }) => {
     return (
         <>
             { projects.map(project => (
                 <li key={project.id} className={`project-box ${project.type === "web" ? "web-box" : ""}`}>
                     {project.type === "web" ?
                         <>
-                            <h2>{project.name}</h2>
-                            <ul className="tech-stack-list">
-                                {project.projectTechStack.map((el, idx) => (
-                                    <li key={idx}>{el}</li>
-                                ))}
-                            </ul>
-                            {/* <div className={`desktop-mockup web1`}></div> */}
-                            <img src={`./src/utils/assets/${project.fileName}`} className="desktop-image" />
-                            <div className={`mobile-mockup web1-mobile`}></div>
+                            <div className="text-box">
+                                <h2>{project.name}</h2>
+                                <ul className="tech-stack-list">
+                                    {project.projectTechStack.map((el, idx) => (
+                                        <li key={idx}>{el}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className="mockups-wrapper">
+                                <img src={`./src/utils/assets/${project.fileName}`} className="desktop-image" />
+                                {project.mobileFileName ? <img src={`./src/utils/assets/${project.mobileFileName}`} className="mobile-image" /> : ""}
+                            </div>
                         </>
                         :
-                        <div className={`art web1`}></div>
+                        <img src={`./src/utils/assets/${project.fileName}`} className="art-image" />
                     }
                     <ProjectHoverComponent projectName={project.projectName} />
                 </li>
